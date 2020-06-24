41c2188f1c3c57e5e858cf73f0ac18946e3e5a74
Galileo Sanchez <galileopy@gmail.com>
2020-06-24 14:27:23 -0400

    include everything

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as fetch from "./fetch";
 import * as react from "./react";
-import * as streams from "./streams";
 import * as unions from "./unions";
+import * as streams from "./streams";
 import * as constants from "./constants";
 
 export default { fetch, react, streams, unions, constants };
