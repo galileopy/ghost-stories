45fc67d56f0efd1e56c12132194283376a5e3206
Galileo Sanchez <galileopy@gmail.com>
2021-01-07 08:57:30 -0300

    Adding folktale/validation render

---
+++ src/react/validation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function (props) {
+  const { validation, Success, Failure } = props;
+  return validation.matchWith({
+    Success(success) {
+      return <Success validation={success} value={success.value} />;
+    },
+    Failure(failure) {
+      return <Failure validation={failure} messages={failure.messages} />;
+    },
+  });
+}
